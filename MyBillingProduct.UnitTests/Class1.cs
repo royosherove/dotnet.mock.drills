69b6d7517cd4fc22692cb6ae4f8e21c273d157c5
royo <royo>
2020-02-23 22:28:54 +0200

    initial commit

---
+++ MyBillingProduct.UnitTests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace MyBillingProduct.UnitTests
+{
+    //rename this class as needed
+    public class Class1
+    {
+        [Test]
+        public void FirstTest()
+        {
+            LoginManager1 lm = new LoginManager1();
+            lm.AddUser("a", "pass");
+           //? 
+        }
+    }
+}
